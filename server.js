68a7313edb168fec30b0842355ea659ad3978974
Joseph <Joseph.carver1@gmail.com>
2025-07-04 18:51:00 -0500

    Refactored to NodeJS

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Serve static files from the "public" folder
+app.use(express.static('public'));
+
+// Default route
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+app.listen(port, () => {
+  console.log(`Website running at http://localhost:${port}`);
+});
